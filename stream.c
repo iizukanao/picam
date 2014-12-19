950f411cd9a463e6b06ec2290a95c2884c542b04
Nao Iizuka <iizuka@kyu-mu.net>
2014-12-19 17:27:02 +0900

    Bump version to 1.2.5

--- stream.c
+++ stream.c
@@ -45,7 +45,7 @@ extern "C" {
 #include "log.h"
 
 #define PROGRAM_NAME     "picam"
-#define PROGRAM_VERSION  "1.2.4"
+#define PROGRAM_VERSION  "1.2.5"
 
 // Audio-only stream is created if this is 1 (for debugging)
 #define AUDIO_ONLY 0
